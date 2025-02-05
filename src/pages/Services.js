15df161d41ed1bbd42f27963ae11ddc338940f73
aj211124 <aaaforajay@gmail.com>
2025-02-05 22:23:25 +0530

    removing unwanted calling

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import Services from "../components/Services";
-import Footer from "../components/Footer";
 
 function ServicesPage() {
   return (
